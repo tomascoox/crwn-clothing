6383844a224e06cc56a9d19e663cf8b39c2ec4c2
Tomas Coox <tomas@joox.se>
2020-07-20 15:00:11 +0200

    update our application with reselect, replace cartselectors with memoized selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import './cart-dropdown.styles.scss';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
+
+import './cart-dropdown.styles.scss';
 
 const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
@@ -16,6 +18,6 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({ cartItems });
+const mapStateToProps = (state) => ({ cartItems: selectCartItems(state) });
 
 export default connect(mapStateToProps)(CartDropdown);
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,4 @@
-import CartActionTypes from './cart-types';
+import CartActionTypes from './cart.types';
 import { addItemToCart } from './cart.utils';
 
 const INITIAL_STATE = {
