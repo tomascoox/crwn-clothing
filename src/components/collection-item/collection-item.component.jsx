60658f5842fdae680e9443ae59bff78efba1d655
Tomas Coox <tomas@joox.se>
2020-07-20 11:24:04 +0200

    created cart-item component and connected our cart drowdown with cartItems reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+
 import { connect } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.actions';
 
 import CustomButton from '../custom-button/custom-button.component';
-import { addItem } from '../../redux/cart/cart.actions';
 
 import './collection-item.styles.scss';
 
